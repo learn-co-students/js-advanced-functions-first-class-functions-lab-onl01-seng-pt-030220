bb9f6b833b1f2148750b3f816f0fe9b77afe44e7
MarcRodrigFelix <m.rodrig.felix@gmail.com>
2020-09-30 23:12:13 -0700

    Done.

--- index.js
+++ index.js
@@ -1,6 +1,36 @@
-const returnFirstTwoDrivers = function(arraysOfDrivers){
-  return arraysOfDrivers
+const returnFirstTwoDrivers = (arraysOfDrivers) => {
+  let firstDrivers = arraysOfDrivers.slice(0,2)
+  return firstDrivers
+};
+
+
+const returnLastTwoDrivers = function(arraysOfDrivers){
+  let lastDrivers = arraysOfDrivers.slice(-2)
+  return lastDrivers
+};
+
+
+const selectingDrivers = [
+  returnFirstTwoDrivers,
+  returnLastTwoDrivers
+];
+
+
+function createFareMultiplier(number){
+  return function fairMultiplier(fare){
+    return number * fare
+  }
+  return fairMultiplier()
+};
+
+
+const fareDoubler = fare => createFareMultiplier(1)(fare) * 2
+
+const fareTripler = fare => createFareMultiplier(1)(fare) * 3
+
+
+
+function selectDifferentDrivers(arraysOfDrivers, driversFunction){
+  return driversFunction(arraysOfDrivers)
 }
 
-
-console.log(returnFirstTwoDrivers(['marcos','vanessa']))
